a2d04b313d867f1cfe13517137515e2b292243f4
victor <vic.emannuel@gmail.com>
2023-11-17 00:20:30 -0300

    .

--- pages/4_dash.py
+++ pages/4_dash.py
@@ -46,7 +46,6 @@ def animation_demo() -> None:
     caixa4.plotly_chart(grafico_tipo_pagamento, use_container_width=True)
     caixa5.plotly_chart(grafico_ranking_cidade, use_container_width=True)
 
-    st.set_page_config(layout="wide") #comando para usar a pagina toda
 
 # st.set_page_config(page_title="Animation Demo", page_icon="📹", layout="wide")
 # st.markdown("# Animation Demo")
@@ -58,5 +57,6 @@ def animation_demo() -> None:
 # )
 
 animation_demo()
+st.set_page_config(layout="wide") #comando para usar a pagina toda
 
 # show_code(animation_demo)
